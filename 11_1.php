97f7ce8b5e5fe7f0a611bf2a03492e22d0551960
Lorenzo <costa.lorenzo98@gmail.com>
2020-06-05 13:33:46 +0200

    primo committ

---
+++ 11_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <title>PHP 11_1</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Lorenzo Costa" >
+    <link rel=stylesheet href=fogliodistile.css>
+  </head>
+  <body>
+      <h1>Pagina realizzata con PHP</h1>
+      <p>Autore della pagina: Lorenzo Costa</p>
+      <?php
+      
+       ?>
+  </body>
+</html>
